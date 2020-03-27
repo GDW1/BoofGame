9e4f2e8ebadb94b1e8dd9bfd381fcaeda631f23c
Home PC <797529@seq.org>
2020-03-26 22:16:58 -0700

    stealth in

---
+++ BoofGame/Assets/Scripts/hide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+    public bool isHiding;
+    private SpriteRenderer invisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHiding = false;
+        invisible = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("h")){
+            invisible.color = new Color(1f,1f,1f,0.5f);
+            isHiding = true;
+        }
+        if(Input.GetKeyUp("h")){
+            invisible.color = new Color(1f,1f,1f,1f);
+            isHiding = false;
+        }
+    }
+}
